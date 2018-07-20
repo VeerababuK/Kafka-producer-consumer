d6352da568786baf6960e42a10c0d17a49a6cfd0
Veerababu Kanumilli <kanumilli.veerababu@gmail.com>
2018-07-20 21:47:21 +0530

    Initial commit

---
+++ src/main/java/com/veera/kafka/SpringBootKafkaExamplesApplication.java
@@ -0,0 +1,12 @@
+package com.veera.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaExamplesApplication.class, args);
+	}
+}
